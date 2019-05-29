7ab3236c7f14e94aec07b8daed784cc47fea27f2
icheikhrouhou <38262569+icheikhrouhou@users.noreply.github.com>
2019-05-29 17:56:45 +0200

    Update lambda_ecs.py

--- lambda_ecs.py
+++ lambda_ecs.py
@@ -5,36 +5,36 @@ def lambda_handler(event, context):
 		
   ecs = boto3.client('ecs')
 	response = ecs.run_task(
-		cluster='cluster_name',
-		taskDefinition='batch',
-    launchType='EC2',
-    tags=[
-      {
-          'key': 'ENV',
-          'value': 'dev'
-      },
-    ],
-	  overrides={
-			'containerOverrides': [
-				{
-					'name': 'batch',
-          'command': [
-                  '/tmp/batch_script.sh',
-          ],
-          'cpu': 123,
-          'memory': 123,
-          'memoryReservation': 123,
-          'taskRoleArn': '',  
-				  'environment': [
-					  {
-							'name': 'ENV',
-							'value': dev
-						},
-					]
-				},
-			]
-		},
-		count=1
+        cluster='cluster_name',
+        taskDefinition='batch',
+        launchType='EC2',
+        tags=[
+        {
+            'key': 'ENV',
+            'value': 'dev'
+        },
+        ],
+        overrides={
+            'containerOverrides': [
+                {
+                    'name': 'batch',
+                    'command': [
+                        '/tmp/batch_script.sh',
+                    ],
+                    'cpu': 123,
+                    'memory': 123,
+                    'memoryReservation': 123,
+                    'taskRoleArn': '',  
+                        'environment': [
+                            {
+                                'name': 'ENV',
+                                'value': dev
+                            },
+                        ]
+                    },
+                ]
+            },
+        count=1
 	)
 
   arn = response["tasks"][0]['taskArn']
